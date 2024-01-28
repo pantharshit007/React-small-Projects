1edc65c22297a9156272e9b2b42b2a02415e91cc
pantharshit007 <20BCS7140@cuchd.in>
2024-01-28 12:15:51 +0530

    inital commit

---
+++ router-studynotion/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold underline">
+      hi mom!
+    </div>
+  )
+}
+
+export default App
