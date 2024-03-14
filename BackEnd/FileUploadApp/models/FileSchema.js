e182596298f43e52769d5502d0910a24c47fe47e
pantharshit007 <20BCS7140@cuchd.in>
2024-03-15 03:32:50 +0530

    fileApp [MAIL] ✨

--- BackEnd/FileUploadApp/models/FileSchema.js
+++ BackEnd/FileUploadApp/models/FileSchema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
     name: {
@@ -15,5 +17,39 @@ const fileSchema = new mongoose.Schema({
         type: String,
     }
 });
+
+// post Middelware
+fileSchema.post('save', async function (doc) {
+    try {
+        //Transporter
+        //TODO: shift to config Folder
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        });
+
+        //send Mail
+        let info = await transporter.sendMail({
+            from: `FileUploadApp @pantHarshit`,
+            to: doc.email,
+            subject: 'New File Uploaded on Cloudinary',
+            html: `<h2>Hey, ${doc.name}!</h2>
+                    <h3>File Uploaded! ✨</h3>
+                    <p>View Here: <a href=${doc.imageUrl}>here 🔗</a></p>`
+        })
+
+        // console.log(info)
+
+    } catch (err) {
+        console.log("Mail Not sent: " + err.message)
+        res.status(400).json({
+            success: false,
+            message: "Mail Not sent: " + err.message,
+        })
+    }
+})
 
 module.exports = mongoose.model('File', fileSchema);
