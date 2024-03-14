028ff2ff33ad6efae498df0fe321f4501c4b8517
pantharshit007 <20BCS7140@cuchd.in>
2024-03-15 01:51:55 +0530

    fileApp [image/ video/ local/ reducedImage] ✅

--- BackEnd/FileUploadApp/controllers/fileUpload.js
+++ BackEnd/FileUploadApp/controllers/fileUpload.js
@@ -5,9 +5,31 @@ function isFileTypeSupported(type, supportedTypes) {
     return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(filename, folder) {
-    const options = { folder };
-    return await cloudinary.uploader.upload(filename.tempFilePath, options);
+async function uploadFileToCloudinary(file, folder, quality = 80) {   // Set a default quality
+    const options = {
+        folder,
+        resource_type: "auto",
+        quality: quality,
+        transformation: [{ quality: quality }] // Apply quality setting 
+    };
+    // if (quality) options.quality = quality;
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+
+    // Check if the file is an image or a video : file object has a property called mimetype
+    const isImage = file.mimetype.startsWith('image');
+    const isVideo = file.mimetype.startsWith('video');
+
+    if (isImage) {
+        return await cloudinary.uploader.upload(file.tempFilePath, options);
+    } else if (isVideo) {
+        //below code provides path also, but its slower and duplication possible
+        // return await cloudinary.uploader.upload(file.tempFilePath, options);
+        //below code doesn't provide path, but its faster and duplication not possible
+        return await cloudinary.uploader.upload_large(file.tempFilePath, options);
+
+    } else {
+        throw new Error('Unsupported file type');
+    }
 }
 
 async function imageUpload(req, res) {
@@ -20,7 +42,7 @@ async function imageUpload(req, res) {
         const fileType = file.name.split('.')[1].toLowerCase();
 
         if (!isFileTypeSupported(fileType, supportingType)) {
-            res.status(404).json({
+            return res.status(404).json({
                 success: false,
                 message: 'File type not supported'
             })
@@ -52,26 +74,98 @@ async function imageUpload(req, res) {
         });
     }
 }
+
 async function videoUpload(req, res) {
     try {
+        const { name, tags, email } = req.body;
+        // console.log(name, tags, email);
+        const file = req.files.videoFile;
+        // console.log(file)
+
+        //validation
+        const supportingType = ['mp4', 'mkv', 'mov'];
+        const fileType = file.name.split('.')[1].toLowerCase();
+
+        //TODO: checking whether video size is greater than 5MB
+        if (!isFileTypeSupported(fileType, supportingType)) {
+            return res.status(404).json({
+                success: false,
+                message: 'File type not supported'
+            })
+        }
+
+        // uploading file to cloudinary after successful validation
+        const response = await uploadFileToCloudinary(file, "FolderApp");
+
+        // Entry in the db
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url
+        })
+
+        res.status(200).json({
+            success: true,
+            imageURL: response.secure_url,
+            message: "Video uploaded successfully on Cloudinary.",
+        })
 
     } catch (err) {
+        console.error("Video upload failed: " + err.message)
         res.status(404).json({
             success: false,
-            message: err.message,
+            message: "Video Upload Failed: " + err.message,
         });
     }
 }
+
 async function imageReducerUpload(req, res) {
     try {
+        const { name, tags, email } = req.body;
+
+        const file = req.files.reduceFile;
+        // console.log(file)
+
+        //validation
+        const supportingType = ['jpg', 'png', 'gif', 'jpeg'];
+        const fileType = file.name.split('.')[1].toLowerCase();
+
+        //TODO: checking whether video size is greater than 5MB
+        if (!isFileTypeSupported(fileType, supportingType)) {
+            return res.status(404).json({
+                success: false,
+                message: 'File type not supported'
+            })
+        }
+
+        // uploading file to cloudinary after successful validation
+        // 3rd value is for quality
+        const response = await uploadFileToCloudinary(file, "FolderApp", 50);
+
+        // Entry in the db
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url
+        })
+
+        res.status(200).json({
+            success: true,
+            imageURL: response.secure_url,
+            message: "Compressed Image uploaded successfully on Cloudinary.",
+        })
 
     } catch (err) {
+        console.error("Reduced Image upload failed: " + err.message)
         res.status(404).json({
             success: false,
-            message: err.message,
+            message: "Reduced Image Upload Failed: " + err.message,
         });
     }
 }
+
 async function localFileUpload(req, res) {
     try {
         if (!req.files || !req.files.file) {
